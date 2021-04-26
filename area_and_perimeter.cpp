130f944a6def8a34225a7df5f48f30f7443fc796
Brian Musembi <brian.musembi@mths.ca>
2021-04-26 14:49:53 +0000

    added area_and_perimeter.cpp

---
+++ area_and_perimeter.cpp
@@ -0,0 +1,29 @@
+// Copyright (c) 2021 Brian Musembi All Rights Reserved
+//
+// Created by: Brian Musembi
+// Created on: 23 April 2021
+// This program calculates the area of a rectangle 73m by 82m
+
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+  int lengthOfRectangle;
+  int widthOfRectangle;
+  int areaOfRectangle;
+  int perimeterOfRectangle;
+
+  std::cout << "Enter the length of the rectangle (mm): ";
+  std::cin >> lengthOfRectangle;
+  std::cout << "Enter the width of the rectangle (mm): ";
+  std::cin >> widthOfRectangle;
+
+  areaOfRectangle = (lengthOfRectangle * widthOfRectangle);
+  perimeterOfRectangle = 2 * (lengthOfRectangle + widthOfRectangle);
+
+  std::cout << "" << std::endl;
+  std::cout << "Area is: " << areaOfRectangle << " mm²." << std::endl;
+  std::cout << "Perimeter is: " << perimeterOfRectangle << " mm." << std::endl;
+  std::cout << std::endl;
+  std::cout << "Done." << std::endl;}
